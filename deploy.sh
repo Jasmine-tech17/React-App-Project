ba91b7a57aa4d5bdf9baadcc10c58294a1e68507
Jasmine-tech17 <jasminechaudhary1997@gmail.com>
2025-04-05 23:43:17 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,14 @@ git reset --hard HEAD
 git clean -fd
 git pull origin dev
 
-# Rebuild containers
+# Stop old containers
 echo "Stopping old containers..."
 docker-compose down
 
+# Free up port 80 if needed
+echo "Freeing up port 80 if occupied..."
+sudo fuser -k 80/tcp || true
+
+# Deploy new containers
 echo "Deploying the Docker container..."
 docker-compose up -d --build
