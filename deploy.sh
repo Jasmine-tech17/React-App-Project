96e1f617bdbffecf34dc659c6584736340f48034
Jasmine-tech17 <jasminechaudhary1997@gmail.com>
2025-04-05 23:16:26 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# Navigate to repo if not already there
+cd "$(dirname "$0")"
+
+# Reset and pull latest code
+git reset --hard HEAD
+git clean -fd
+git pull origin dev
+
+# Rebuild containers
 echo "Stopping old containers..."
 docker-compose down
 
