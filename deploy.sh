d7d50422491350e42e969343067ffcec11b75cb7
Jasmine-tech17 <jasminechaudhary1997@gmail.com>
2025-04-06 00:39:02 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@
 # Navigate to repo if not already there
 cd "$(dirname "$0")"
 
-# Reset and pull latest code
+# Clean workspace
 git reset --hard HEAD
 git clean -fd
-git pull origin dev
 
 # Stop old containers
 echo "Stopping old containers..."
@@ -14,7 +13,8 @@ docker-compose down
 
 # Free up port 80 if needed
 echo "Freeing up port 80 if occupied..."
-sudo -n /usr/sbin/fuser -k 80/tcp || echo "Port 80 was not occupied or permission denied"
+sudo -n /usr/bin/fuser -k 80/tcp || echo "Port 80 was not occupied or permission denied"
+
 
 # Deploy new containers
 echo "Deploying the Docker container..."
